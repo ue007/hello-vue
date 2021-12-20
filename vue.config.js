1277a8bf7b502d2c9c8958f38ba2a77206a90c63
ue007 <ue007@webgpu.info>
2021-12-20 23:31:11 +0800

    release

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
 	// baseUrl: process.env.NODE_ENV === 'production' ? './' : '/'
 	publicPath: process.env.NODE_ENV === "production" ? "/public/" : "/docs/",
 	// 输出文件目录：在npm run build时，生成文件的目录名称
-	outputDir: "dist",
+	outputDir: "docs",
 	// 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
 	assetsDir: "assets",
 	// 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度
